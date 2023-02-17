2d9d41df7b0e2dd548f53b483882a84afc6a6b10
allysonaggp <allysonaggp@gmail.com>
2023-02-17 18:57:38 -0300

    fist commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+    gato gato = new gato();
+
+        System.out.println(gato);
+
+    /*public static void main(String[] args) {
+    int a = 5;
+    int b = 3;
+        System.out.println("hello world " + (a+b));*/
+     }
+}
